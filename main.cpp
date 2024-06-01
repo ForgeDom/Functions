23248ccd1b894fa640652f94838cbcf5d3cfedad
ForgeDom <maxstill2006@gmail.com>
2024-06-01 20:58:09 +0300

    +

--- main.cpp
+++ main.cpp
@@ -2,8 +2,19 @@
 #include <ctime>
 using namespace std;
 
+int Sum(int a, int b) {
+	int sum = 0;
+	for (int i = a; i <= b; i++) {
+		sum += i;
+	}
+	return sum;
+}
+
 int main()
 {
-
+	int a, b;
+	cin >> a >> b;
+	int res = Sum(a, b);
+	cout << res;
 	return 0;
 }
