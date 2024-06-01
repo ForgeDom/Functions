8b5563f4fc35782a3f2682c550709b19bbc9338c
ForgeDom <maxstill2006@gmail.com>
2024-06-01 17:56:18 +0300

    Lab ex2 done

--- main.cpp
+++ main.cpp
@@ -2,8 +2,27 @@
 #include <ctime>
 using namespace std;
 
+int factorial(int a) {
+	int b = 1;
+	for (int i = 1; i <= a; ++i) {
+		b *= i;
+	}
+	return b;
+}
+
 int main()
 {
+	int a;
+	cout << "Enter the value:" << endl;
+	cin >> a;
 
+	if (a < 0) {
+		cout << "Factorial cant be negative" << endl;
+	}
+	else {
+		int res = factorial(a); 
+		cout << res;
+	}
+	
 	return 0;
 }
