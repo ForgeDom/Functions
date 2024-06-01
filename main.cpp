72d1d27d92500d35a83450ac1b3b7f543c3af184
ForgeDom <maxstill2006@gmail.com>
2024-06-01 18:23:09 +0300

    Lab ex6 done

--- main.cpp
+++ main.cpp
@@ -2,8 +2,30 @@
 #include <ctime>
 using namespace std;
 
+bool Check(int a) {
+	if (a < 0) {
+		return false;
+	}
+	else if (a > 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 int main()
 {
+	int a;
+	cin >> a;
+
+	bool res = Check(a);
+
+	if (res) {
+		cout << a << " is positive." << endl;
+	}
+	else {
+		cout << a << " is zero or negative." << endl;
+	}
 
 	return 0;
 }
