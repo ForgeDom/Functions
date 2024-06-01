ac699e10ddbf0d2e694e8149ed7cf4f9bee2746d
ForgeDom <maxstill2006@gmail.com>
2024-06-01 18:04:58 +0300

    Lab ex3 done

--- main.cpp
+++ main.cpp
@@ -2,8 +2,30 @@
 #include <ctime>
 using namespace std;
 
+bool Check(int a) {
+	if (a < 0) {
+		return false;
+	}
+	for (int i = 2; i <= a / 2; ++i) {
+		if (a % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
 int main()
 {
+	int a;
+	cout << "Enter the value: ";
+	cin >> a;
+
+	if (Check(a)) {
+		cout << "E prostum" << endl;
+	}
+	else {
+		cout << "Ne e prostum" << endl;
+	}
 
 	return 0;
 }
