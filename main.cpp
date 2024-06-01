20aac18e728e6bdbd10b520e25dc8ab5d7458fa8
ForgeDom <maxstill2006@gmail.com>
2024-06-01 17:45:17 +0300

    Lab ex1 done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
 
-int main()
-{
+void rectangle(int N, int K) {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < K; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
 
-	return 0;
+int main() {
+    int N, K;
+    cout << "Enter height (N): ";
+    cin >> N;
+    cout << "Enter width (K): ";
+    cin >> K;
+
+    rectangle(N, K);
+
+    return 0;
 }
