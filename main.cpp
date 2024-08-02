fb418e801e781d86d37c044d2b4f929a0e775c94
Max <maxstill2006@gmail.com>
2024-08-02 21:46:34 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
-#include <ctime>
+
 using namespace std;
 
-int main()
-{
+bool isPerfect(int number) {
+    int sum = 1; 
+    for (int i = 2; i <= number / 2; ++i) {
+        if (number % i == 0) {
+            sum += i;
+        }
+    }
 
-	return 0;
+    return sum == number;
+}
+
+int findPerfectNumbersInRange(int start, int end, int perfectNumbers[], int maxSize) {
+    int count = 0;
+
+    for (int i = start; i <= end && count < maxSize; ++i) {
+        if (isPerfect(i)) {
+            perfectNumbers[count++] = i;
+        }
+    }
+
+    return count; 
+}
+
+int main() {
+    int start, end;
+    const int maxPerfectNumbers = 10; 
+    int perfectNumbers[maxPerfectNumbers];
+
+    cout << "Enter beginning of the interval: ";
+    cin >> start;
+    cout << "Enter end of the interval: ";
+    cin >> end;
+
+    int count = findPerfectNumbersInRange(start, end, perfectNumbers, maxPerfectNumbers);
+
+    cout << "Perfect number are from " << start << " to " << end << ": ";
+    for (int i = 0; i < count; ++i) {
+        cout << perfectNumbers[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
